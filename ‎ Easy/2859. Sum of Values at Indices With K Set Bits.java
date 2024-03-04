0b36d24f9b2ab4e339d2652110404560b0260289
DamiennFl <damien.flutre@gmail.com>
2024-03-04 12:06:09 -0800

    2859

---
+++ ‎ Easy/2859. Sum of Values at Indices With K Set Bits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int sumIndicesWithKSetBits(List<Integer> nums, int k) {
+        int total = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            String currBinary = Integer.toBinaryString(i);
+            char[] currBinaryArray = currBinary.toCharArray();
+            int binaryCount = 0;
+            for(char c : currBinaryArray) {
+                if(c == '1') binaryCount++;
+            }
+            if (binaryCount == k) total += nums.get(i);
+        }
+        return total;
+    }
+}
